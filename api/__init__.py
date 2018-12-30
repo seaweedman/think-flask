11f97b0e882a3f9a4279709863a58f053d4fa5d4
seaweedman <seaweedman@163.com>
2018-12-30 09:48:30 +0800

    :baby_chick: router group by blueprint

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restful import Api
 
 def create_app():
@@ -6,6 +6,9 @@ def create_app():
 
     from api.v1 import create_resource_v1
 
-    create_resource_v1(Api(app))
+    blueprint_v1 = Blueprint('api_v1', __name__)
+    api_v1 = Api(blueprint_v1)
+    create_resource_v1(api_v1)
+    app.register_blueprint(blueprint_v1, url_prefix='/v1')
 
     return app
